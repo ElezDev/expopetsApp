a8f1356671d4ec2ee6067ffb48cab0cb7ba23392
ElezDev <edwinledezma2013@gmail.com>
2025-01-18 13:25:10 -0500

    Actualizar la URL base de la API a la producción

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,3 +1,3 @@
-const BASE_URL = 'http://192.168.1.16:8000/api/';
-
+// const BASE_URL = 'http://192.168.1.16:8000/api/';
+const BASE_URL = 'https://findpestbackend-production.up.railway.app/api/';
 export default BASE_URL;
